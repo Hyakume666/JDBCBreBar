872d279cfdc249b14df4ae0aabc4cce3ee5c1d9d
Loic Barthoulot <loic.barthoulot@he-arc.ch>
2025-10-25 08:56:21 +0200

    Correction of javadoc

--- src/main/java/ch/hearc/ig/guideresto/persistence/ConnectionUtils.java
+++ src/main/java/ch/hearc/ig/guideresto/persistence/ConnectionUtils.java
@@ -10,8 +10,8 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 /**
- * Provide helper methods to deal with database connections.
- * Ideally, this should also manage connection pools in a bigger application.
+ * Fournit des méthodes utilitaires pour gérer les connexions à la base de données.
+ * Idéalement, cette classe devrait également gérer les pools de connexions dans une application plus importante.
  *
  * @author arnaud.geiser
  * @author alain.matile
@@ -22,17 +22,24 @@ public class ConnectionUtils {
 
     private static Connection connection;
 
+    /**
+     * Retourne une connexion à la base de données.
+     * Les informations de connexion sont chargées depuis le fichier de ressources {@code resources/database.properties}.
+     * Si aucune connexion n’existe encore ou si elle a été fermée, une nouvelle est créée.
+     *
+     * @return un objet {@link Connection} actif vers la base de données.
+     */
     public static Connection getConnection() {
         try {
-            // Load database credentials from resources/database.properties
+            // Charger les identifiants de la base de données depuis resources/database.properties
             ResourceBundle dbProps = ResourceBundle.getBundle("database");
             String url = dbProps.getString("database.url");
             String username = dbProps.getString("database.username");
             String password = dbProps.getString("database.password");
 
-            logger.info("Trying to connect to user schema '{}' with JDBC string '{}'", username, url);
+            logger.info("Tentative de connexion au schéma utilisateur '{}' avec la chaîne JDBC '{}'", username, url);
 
-            // Initialize a connection if required
+            // Initialiser une connexion si nécessaire
             if (ConnectionUtils.connection == null || ConnectionUtils.connection.isClosed()) {
                 Connection connection = DriverManager.getConnection(url, username, password);
                 connection.setAutoCommit(false);
@@ -45,6 +52,9 @@ public class ConnectionUtils {
         return ConnectionUtils.connection;
     }
 
+    /**
+     * Ferme proprement la connexion à la base de données si elle est ouverte.
+     */
     public static void closeConnection() {
         try {
             if (ConnectionUtils.connection != null && !ConnectionUtils.connection.isClosed()) {
